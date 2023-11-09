1ca526bafc92ec504f3a2f0f740cf7f6c9c67622
Jeff Hykin <jeff.hykin@gmail.com>
2023-11-08 20:24:25 -0600

    add json to scratchwork

--- scratch_work/nix_to_xml.js
+++ scratch_work/nix_to_xml.js
@@ -1,8 +1,16 @@
 import { FileSystem } from "https://deno.land/x/quickr@0.6.51/main/file_system.js"
-import { nixFileToXml } from "../tools/parsing.js"
+import { nixFileToXml, parse } from "../tools/parsing.js"
 
 await Promise.all(
     (await FileSystem.listFilePathsIn(FileSystem.thisFolder)).filter(each=>each.endsWith(".nix")).map(
         eachPath=>nixFileToXml(eachPath)
+    )
+)
+await Promise.all(
+    (await FileSystem.listFilePathsIn(FileSystem.thisFolder)).filter(each=>each.endsWith(".nix")).map(
+        eachPath=>FileSystem.write({
+            path: eachPath+".json",
+            data: JSON.stringify(parse(FileSystem.sync.read(eachPath)).rootNode,0,2),
+        })
     )
 )
