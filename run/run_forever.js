8fc2209cec92753cf06d0e5653e23188d612c2fd
Jeff Hykin <jeff.hykin@gmail.com>
2026-02-06 01:42:39 -0800

    -

--- run/run_forever.js
+++ run/run_forever.js
@@ -14,6 +14,8 @@ while (true) {
     await $`claude --allowedTools "Task,Edit,Read,Update,Write,WebFetch,WebSearch,Glob,Grep,Write(./**/*),Bash(*)" -p ${`
         You are tasked with faithfully creating a nix runtime in JavaScript. A system that can execute nix code by translating it to javascript, and running that JS. The main/runtime.js is the engine for already-translated nix code. It needs to have a working implementation of every nix builtin and operator. You can use deno with URL imports, and esm.sh helps with npm, but you are not allowed to use jsr or npm imports directly. Read prompt.md for context for where the previous agent left off, and add detail of what needs to be done to prompt.md. DO NOT REPORT WHAT YOU HAVE DONE, ONLY REPORT WHAT REMAINS TO BE DONE. Now please resume the work
     `} &> .claude/${increment++}.log`
+    await $`git add -A`
+    await $`git commit -m ${`CLAUDE: change ${increment}`}`
 
     await $`claude --allowedTools "Task,Edit,Read,Update,Write,WebFetch,WebSearch,Glob,Grep,Write(./**/*),Bash(*)" -p ${`
             There is another bot running, but it tends to run off the rails. You are a coach that keeps them on track. Please edit the prompt.md file to enforce the following:
@@ -28,6 +30,8 @@ while (true) {
             
             - If there are checkboxes (✅) remove those items and anything else that looks like an achievement.
             - Evaluate if those rules are being followed. If not, enforce them yourself by editing the prompt (add the statement to the top, remove achievements, etc). Then edit the prompt.md file to encourage the bot to follow the rules.
+            - Remind the agent that implementations should be based on nix documentation and behavior. E.g. read while working on builtins.fetchTarball read https://noogle.dev/f/builtins/fetchTarball 
+            - Remind the agent they are allowed to use npm modules but only if they are done through https://esm.sh/NPM_MODULE_NAME (which doesn't always work)
         `} &> .claude/${increment++}.log`
     await $`git add -A`
     await $`git commit -m ${`CLAUDE: change ${increment}`}`
